1e0346b7603f8ab5656148abdc30758f69747c7b
Den <anton.tim4enko.2014@gmail.com>
2020-04-02 23:10:29 +0300

    feature/swagger

--- tenanter/urls.py
+++ tenanter/urls.py
@@ -15,10 +15,21 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Tenanter API",
+        default_version='v1',
+    ),
+    public=True,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include("tenanter.apps.user.urls")),
-    path('api/', include("tenanter.apps.flat.urls")),
-    path('api/', include("tenanter.apps.bills.urls")),
+    path('api/', schema_view.with_ui()),
+    path('', include("tenanter.apps.user.urls")),
+    path('', include("tenanter.apps.flat.urls")),
+    path('', include("tenanter.apps.bills.urls")),
 ]
