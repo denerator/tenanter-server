57572cb74a4a682191a43cf32432bbf8850d8ef6
Den <denisworkman3@gmail.com>
2020-06-05 01:07:14 +0300

    feature/days-before-payment

--- tenanter/apps/flat/serializers.py
+++ tenanter/apps/flat/serializers.py
@@ -12,10 +12,33 @@ class TenantSerializer(serializers.ModelSerializer):
 
 
 class FlatSerializer(serializers.ModelSerializer):
+    tenant = TenantSerializer(read_only=True)
+
+    days_before_payment = serializers.SerializerMethodField(
+        'get_days_before_payment')
 
     class Meta:
         model = Flat
-        fields = ['id', 'address', 'owner']
+        fields = ['id', 'address', 'owner', 'days_before_payment', 'tenant']
+
+    def get_days_before_payment(self, obj):
+        days_before_payment = 0
+        if not hasattr(obj, 'tenant'):
+            return days_before_payment
+
+        today = datetime.date.today()
+        month = today.month
+
+        if obj.tenant.payment_day < today.day:
+            month += 1
+
+        payment_date = f'{today.year}-{month}-{obj.tenant.payment_day}'
+        return self.days_between_today(payment_date, today)
+
+    def days_between_today(self, date, today):
+        d1 = datetime.datetime.strptime(today.isoformat(), "%Y-%m-%d")
+        d2 = datetime.datetime.strptime(date, "%Y-%m-%d")
+        return abs((d2 - d1).days)
 
 
 class FlatWithTenantSerializer(serializers.ModelSerializer):
